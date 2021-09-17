02ecc3871d076a9ff83299fdc4452981ebb0860c
YJ17 <pl3434on@gmail.com>
2021-09-17 19:59:22 +0900

    Homework1
    실습과제1_Homework1

---
+++ prog_home1/src/Homework1.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+
+        int num = 0;
+        int total = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.printf("정수를 입력하세요 : ");
+
+        num = scan.nextInt();
+        total += num;
+
+        System.out.println("현재까지 입력된 정수의 합은 " + total +"입니다.");
+
+
+        System.out.printf("정수를 입력하세요 : ");
+
+        num = scan.nextInt();
+        total += num;
+
+        System.out.println("현재까지 입력된 정수의 합은 " + total +"입니다.");
+
+
+        System.out.printf("정수를 입력하세요 : ");
+
+        num = scan.nextInt();
+        total += num;
+
+        System.out.println("현재까지 입력된 정수의 합은 " + total +"입니다.");
+
+
+        System.out.printf("정수를 입력하세요 : ");
+
+        num = scan.nextInt();
+        total += num;
+
+        System.out.println("현재까지 입력된 정수의 합은 " + total +"입니다.");
+
+
+        System.out.printf("정수를 입력하세요 : ");
+
+        num = scan.nextInt();
+        total += num;
+
+        System.out.println("현재까지 입력된 정수의 합은 " + total +"입니다.");
+    }
+
+}
